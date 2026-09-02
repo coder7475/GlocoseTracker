04fec7f8c9f0512f3e9e0a61c6d7dd6dc7503635
coder7475 <robiulhossain7475@gmail.com>
2026-09-02 21:16:04 +0600

    feat: create grid with cards for macros

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,14 @@
-import { Text, View, StyleSheet } from "react-native";
-import { globalStyles, colors } from "@/styles/global";
+import { Text, View } from "react-native";
+import { globalStyles } from "@/styles/global";
 import HomeHeader from "@/components/HomeHeader";
-import { Link } from "expo-router";
+import MacroGrid from "@/components/MacroGrid";
 
 export default function HomeScreen() {
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
       <HomeHeader />
-      <Link href="/meals" style={{ color: '#007bff', fontSize: 18 }}>Go To meals</Link>
+      <MacroGrid />
     </View>
   );
 }
