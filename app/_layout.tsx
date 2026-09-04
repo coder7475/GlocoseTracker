1bca90f4e4863bbb6c925f9df77a0c69e2d4ad13
coder7475 <robiulhossain7475@gmail.com>
2026-09-04 21:03:37 +0600

    feat: upgrade to Expo SDK 57, restructure to src/ layout, remove @react-navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,4 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import { Stack, DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import 'react-native-reanimated';
