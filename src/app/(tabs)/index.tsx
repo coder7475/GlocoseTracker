3e865e7fd9733ed7dc04594b0b6261e7a72f7a58
coder7475 <robiulhossain7475@gmail.com>
2026-09-08 20:02:38 +0600

    feat: add recent meals delete functionality

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -35,7 +35,7 @@ export default function HomeScreen() {
       <Text style={globalStyles.title}>Glocose Tracker</Text>
       <HomeHeader />
       <MacroGrid meals={meals} />
-      <RecentMeals meals={meals} />
+      <RecentMeals meals={meals} onDelete={loadMeals} />
     </ScrollView>
   );
 }
--- src/components/RecentMeals.tsx
+++ src/components/RecentMeals.tsx
@@ -5,9 +5,10 @@ import MealItem from './MealItem';
 
 type RecentMealsProps = {
   meals: Meal[];
+  onDelete: () => void;
 };
 
-export default function RecentMeals({ meals }: RecentMealsProps) {
+export default function RecentMeals({ meals, onDelete }: RecentMealsProps) {
   return (
     <View style={{ marginTop: 30 }}>
       <Text style={globalStyles.sectionTitle}>Recent Meals</Text>
@@ -18,12 +19,14 @@ export default function RecentMeals({ meals }: RecentMealsProps) {
           .slice(0, 5)
           .map((meal: Meal) => (
             <MealItem
+              id={meal.id}
               key={meal.id}
               name={meal.name}
               calories={meal.calories}
               protein={meal.protein}
               carbs={meal.carbs}
               fat={meal.fat}
+              onDelete={onDelete}
             />
           ))
       )}
