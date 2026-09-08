8ab15e01de497f1b5b4d8be5b8807d8a4c7fca0e
coder7475 <robiulhossain7475@gmail.com>
2026-09-08 19:13:54 +0600

    feat: add recent meals to storage

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -5,17 +5,24 @@ import HomeHeader from "@/components/HomeHeader";
 import MacroGrid from "@/components/MacroGrid";
 import RecentMeals from "@/components/RecentMeals";
 import { getMeals, type Meal } from "@/storage/meals";
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { useFocusEffect } from "expo-router";
 
 export default function HomeScreen() {
   const insets = useSafeAreaInsets();
-  const [meals, setMeals] = useState<Meal[]>();
+  const [meals, setMeals] = useState<Meal[]>([]);
 
   const loadMeals = async () => {
     const data = await getMeals();
     setMeals(data);
     console.log("Loaded Meals: ", data);
-  }
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadMeals();
+    }, [])
+  )
 
   return (
     <ScrollView
@@ -28,7 +35,7 @@ export default function HomeScreen() {
       <Text style={globalStyles.title}>Glocose Tracker</Text>
       <HomeHeader />
       <MacroGrid />
-      <RecentMeals />
+      <RecentMeals meals={meals} />
     </ScrollView>
   );
 }
